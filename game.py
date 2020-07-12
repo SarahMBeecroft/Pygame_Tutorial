2bbeacb6ad940a12a021ee3d12f3c1f05cd41425
Sarah B <smblair87@gmail.com>
2020-07-11 18:37:52 -0700

    More game setup

--- game.py
+++ game.py
@@ -1,56 +1,70 @@
 import pygame
+import os
+import time
+import random
+# Initializes font
+pygame.font.init()
 
-# Initializes the pygame
-pygame.init()
 
-# Creates the game screen (width, height) or (x-axis, y-axis)
-screen = pygame.display.set_mode((800, 600))
+# Sets width and height, and creates window variable 
+WIDTH, HEIGHT = 850, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # Title and Icon
-pygame.display.set_caption('Space Invaders')
-icon = pygame.image.load('images/spaceship.png')
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load(os.path.join("assets", "spaceship.png"))
 pygame.display.set_icon(icon)
 
-# Player icon
-playerImg = pygame.image.load('images/player.png')
+# Load images
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
 
-# Where we are positioning player on x-axis 
-playerX = 370
-# Where we are positioning player on y-axis 
-playerY = 480
-#
-playerX_change = 0
+# Player ship
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
 
-# Player function
-def player(x, y):
-  # blit method draws image of player onto screen, along with x and y coordinates 
-  screen.blit(playerImg, (x, y))
+# Lasers
+RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
+# Background, scaled to width and height
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("assets", "spacebackground.jpg")), (WIDTH, HEIGHT))
 
-# Game loop, infinite loop that makes sure game is always running
-running = True
-while running:
+# Game function 
+def main():
+  run = True
+  FPS = 60 
+  level = 1
+  lives = 3
+  main_font = pygame.font.SysFont("ariel", 50)
+  # Clock object
+  clock = pygame.time.Clock()
 
-  # Sets color of game background with RGB
-  screen.fill((55,4,185))
+  def redraw_window():
+    # Refreshes the display with updated images at location defined (x-axis: 0, y-axis: 0)
+    WIN.blit(BACKGROUND, (0,0))
+    # Draws text
+    lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+    level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
 
-  # Prevents game from hanging
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
+    # Puts text onto screen with blit
+    WIN.blit(lives_label, (10, 10))
+    WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+    
 
-    # Keystroke event to move player, checks to see if right or left
-    if event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_LEFT:
-        print('Left arrow is pressed')
-      if event.key == pygame.K_RIGHT:
-        print('Right arrow is pressed')
-    if event.type == pygame.KEYUP:
-      if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-        print('Keystroke has been released')
+    pygame.display.update()
 
-  # Calls player function 
-  player(playerX, playerY)
+  while run:
+    # Ensure game runs at clock speed of set FPS
+    clock.tick(FPS)
+    redraw_window()
 
-  # Updates game display 
-  pygame.display.update()
+    for event in pygame.event.get():
+      # Game will stop running when window is closed 
+      if event.type == pygame.QUIT:
+        run = False
+
+main()
+
